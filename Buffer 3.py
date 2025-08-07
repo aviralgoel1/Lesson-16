f963c11971ec65bfc923b2b47477dfc00060cf35
aviralgoel1 <aviralgoel1@gmail.com>
2025-08-07 22:53:48 +0100

    hcf

--- Buffer 3.py
+++ Buffer 3.py
@@ -2,11 +2,12 @@
 while True:
     q=int(input("enter number: "))
     p=int(input("enter number: "))
-    x=(p,q)
+    r=int(input("enter number: "))
+    x=(p,q,r)
     low=min(x)
     high=max(x)
     a=()
     for i in range (1,low+1):
-        if low%i==0 and high%i==0:
+        if q%i==0 and p%i==0 and r%i==0:
             a=(i)
     print ("hcf= ", a)
